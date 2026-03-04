e21d17c064a724c6a466ebc8a8b62873303e2f89
diglet48 <diglet48@gmail.com>
2026-03-04 20:53:04 +0100

    Bump version to v1.2.3

--- src/bsp/config_version.h
+++ src/bsp/config_version.h
@@ -3,7 +3,7 @@
 
 constexpr int focstim_api_version_major = 1;
 constexpr int focstim_api_version_minor = 2;
-constexpr int focstim_api_version_revision = 2;
+constexpr int focstim_api_version_revision = 3;
 static constexpr char focstim_api_branch_name[] = "main";   // mainline restim requires this to be "main".
 static constexpr char focstim_api_comment[] = "";           // can be whatever.
 
